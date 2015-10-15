fbb5b925d1930cfdf7e10c85f449d20a71028a58
Dan Kling <kling@towermarketing.net>
2015-10-15 15:55:53 -0400

    slider text

--- wp-content/themes/big_2015/content-wireless-services.php
+++ wp-content/themes/big_2015/content-wireless-services.php
@@ -4,7 +4,7 @@
 	<div class="box box--service col-3">
 		<?php the_sub_field('industry_svg'); ?>
 		<h2><?php the_sub_field('industry_title'); ?></h2>
-		<p><?php the_sub_field('industry_text'); ?></p>
+		<p><?php the_sub_field('industry_text'); ?>[...]</p>
 		<a href="<?php the_sub_field('industry_link'); ?>" class="button">Learn More <span>&rsaquo;</span></a>
 	</div>
 	<?php endwhile ?>
