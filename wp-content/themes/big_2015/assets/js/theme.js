86ae632caabafb6a0aafca529314449731ab47c8
Dan Kling <kling@towermarketing.net>
2015-10-15 13:44:03 -0400

    sliders and textures

--- wp-content/themes/big_2015/assets/js/theme.js
+++ wp-content/themes/big_2015/assets/js/theme.js
@@ -67,6 +67,7 @@ $(document).ready(function() {
 		itemsMobile : [600,1],
 		pagination : true,
 		paginationNumbers: false,
+		transitionStyle : "fade"
 	});
 
 	$('.contact__dropdown').on('click', function(){
